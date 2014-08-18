c5219adb9a2aace6497cbf9f9fdc28aed63393d2
Umang Patel <umang@paytango.com>
2014-08-18 03:51:01 -0700

    added animations; updated code style w/ single quotes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-var checkinApp = angular.module('checkinApp', ["angular-growl"]);
+var checkinApp = angular.module('checkinApp', ['angular-growl', 'ngAnimate']);
 
 checkinApp.config(['growlProvider', function(growlProvider) {
-  growlProvider.globalTimeToLive(5000);
+  growlProvider.globalTimeToLive(3000);
 }]);
 
 checkinApp.controller('CheckInCtrl', ['$scope', '$http', '$timeout', 'growl',
@@ -12,7 +12,7 @@ function($scope, $http, $timeout, growl) {
   
   $scope.getSemester = function() {
     var today = new Date();
-    var season = today.getMonth() < 7 ? "Spring" : "Fall";
+    var season = today.getMonth() < 7 ? 'Spring' : 'Fall';
     return season + ' ' + today.getFullYear();
   }
   
@@ -22,20 +22,22 @@ function($scope, $http, $timeout, growl) {
       method: 'GET',
       url: 'lib/cmu_ldap.php',
       params: { andrew_id: $scope.andrew_id }
-    }).
-    success(function(data, status, headers, config) {
+    })
+    .success(function(data, status, headers, config) {
       $scope.checkIns.push(data);
-      $scope.andrew_id = "";
+      $scope.andrew_id = '';
+      growl.addSuccessMessage('Successfully checked in!');
+      $scope.processing = false;
+      $timeout(function(){
+        document.getElementById('andrew_id').focus();
+      }, 0);      
+    })
+    .error(function(data, status, headers, config) {
+      growl.addErrorMessage(data);
       $scope.processing = false;
       $timeout(function(){
         document.getElementById('andrew_id').focus();
       }, 0);
-      growl.addSuccessMessage("Successfully checked in!");
-      
-    }).
-    error(function(data, status, headers, config) {
-      $scope.processing = false;
-      growl.addErrorMessage(data);
     });
   }
   
