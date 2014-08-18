24a84a8b899db124ec54dacde91c426d5a329430
Umang Patel <umang@paytango.com>
2014-08-18 04:20:13 -0700

    added error outline and fixed growl animation issue

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 var checkinApp = angular.module('checkinApp', ['angular-growl', 'ngAnimate']);
 
-checkinApp.config(['growlProvider', function(growlProvider) {
+checkinApp.config(['growlProvider', '$animateProvider', function(growlProvider, $animateProvider) {
+  // Only animate the growl notifications
+  $animateProvider.classNameFilter(/growl-item/);
   growlProvider.globalTimeToLive(3000);
 }]);
 
@@ -8,6 +10,7 @@ checkinApp.controller('CheckInCtrl', ['$scope', '$http', '$timeout', 'growl',
 function($scope, $http, $timeout, growl) {
   
   $scope.checkIns = [];
+  $scope.error = "";
   $scope.processing = false;
   
   $scope.getSemester = function() {
@@ -18,26 +21,28 @@ function($scope, $http, $timeout, growl) {
   
   $scope.addCheckIn = function() {
     $scope.processing = true;
+    $scope.error = "";
     $http({
       method: 'GET',
       url: 'lib/cmu_ldap.php',
       params: { andrew_id: $scope.andrew_id }
     })
     .success(function(data, status, headers, config) {
-      $scope.checkIns.push(data);
-      $scope.andrew_id = '';
-      growl.addSuccessMessage('Successfully checked in!');
-      $scope.processing = false;
-      $timeout(function(){
-        document.getElementById('andrew_id').focus();
-      }, 0);      
-    })
-    .error(function(data, status, headers, config) {
-      growl.addErrorMessage(data);
       $scope.processing = false;
       $timeout(function(){
         document.getElementById('andrew_id').focus();
       }, 0);
+      $scope.checkIns.push(data);
+      $scope.andrew_id = '';
+      growl.addSuccessMessage('Successfully checked in!');
+    })
+    .error(function(data, status, headers, config) {
+      $scope.processing = false;
+      $scope.error = data;
+      $timeout(function(){
+        document.getElementById('andrew_id').focus();
+      }, 0);
+      growl.addErrorMessage(data);
     });
   }
   
